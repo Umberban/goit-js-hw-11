f15282aec6ae7b3f17796965350317f65607cd53
Umberban <romanovborysv@gmail.com>
2023-02-05 23:01:34 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-API_KEY = '33396171-581c3cabf7afd0272218aaf17';
+const API_KEY = '33396171-581c3cabf7afd0272218aaf17';
 import axios from 'axios';
 function  Request(searchRequest,page){
-return axios.get(`https://pixabay.com/api/?key=33396171-581c3cabf7afd0272218aaf17&q=${searchRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)}
+return axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${searchRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)}
 export {Request};
